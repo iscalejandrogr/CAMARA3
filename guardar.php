451e9781042830ca9d60885cc154e1ffc5003d24
alex1009 <i.s.c.alejandro.gr@gmail.com>
2016-05-05 22:24:39 -0500

    publicar

---
+++ guardar.php
@@ -0,0 +1,24 @@
+<?php
+
+//Se Agrega el archivo que contine la conexion a la BD
+include "conexion.php";
+
+$nom=$_POST['nomb'];
+$dir=$_POST['direc'];
+$rfc=$_POST['Rfc'];
+$tel=$_POST['telef'];
+$clasif=$_POST['classif'];
+$lat=$_POST['latit'];
+$longt=$_POST['longit'];
+$foto=$_POST['Foto'];
+
+$sql="INSERT INTO servicios VALUES('','$nom','$dir','$rfc','$tel','$lat','$longt','$foto','$clasif')";
+$res=mysql_query($sql,$conexion);
+if($res){
+	echo "Los datos se registraron de manera exitosa";
+	echo "nombre:".$nom." Dirección:".$dir." RFC:".$rfc." Teléfono:".$tel." Clasificación:".$clasif." Latitud:".$lat." Longitud:".$longt." Foto:".$foto;
+	}else{
+		  echo "Se produjo un error al momento de registar los datos:".mysql_error();
+		}
+mysql_close($conexion);
+?>
