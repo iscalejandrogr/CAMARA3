33126de0a2578a4782f9745c658399b4469ddd71
alex1009 <i.s.c.alejandro.gr@gmail.com>
2016-05-09 14:22:05 -0500

    conexion remota
    xc

--- conexion.php
+++ conexion.php
@@ -1,14 +1,14 @@
 <?php
 
-$usario="root";
+/*$usario="root";
 $passwd="alex";
 $servidor="localhost";
 $BD="serviciosLibres";
-
-/*$usario="n260m_14426545";
+*/
+$usario="n260m_14426545";
 $passwd="drag0n";
 $servidor="sql206.260mb.net";
-$BD="n260m_14426545_pcatual";*/
+$BD="n260m_14426545_servicios";
 
 $conexion=mysql_connect($servidor,$usario,$passwd)or die("Error al conectarse a la base de datos:".mysql_error());
 mysql_select_db($BD,$conexion);
