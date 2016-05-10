0a0a97afceb82d15fb50f9746b7de5bf9f87163f
alex1009 <i.s.c.alejandro.gr@gmail.com>
2016-05-10 08:49:07 -0500

    Cambio en la notifiacion

--- conexion.php
+++ conexion.php
@@ -3,12 +3,17 @@
 /*$usario="root";
 $passwd="alex";
 $servidor="localhost";
-$BD="serviciosLibres";
-*/
+$BD="serviciosLibres";*/
+
 $usario="n260m_14426545";
 $passwd="drag0n";
 $servidor="sql206.260mb.net";
 $BD="n260m_14426545_servicios";
+
+/*$usario="u726031340_alex";
+$passwd="drag0n";
+$servidor="	mysql.hostinger.mx";
+$BD="	u726031340_libre";*/
 
 $conexion=mysql_connect($servidor,$usario,$passwd)or die("Error al conectarse a la base de datos:".mysql_error());
 mysql_select_db($BD,$conexion);
